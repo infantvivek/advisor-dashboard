1229eec8720f3acb69202e407507f10c6ffd3add
infantvivek <infantvivek@gmail.com>
2026-03-05 09:18:29 +0530

    Update sheets.ts

--- lib/sheets.ts
+++ lib/sheets.ts
@@ -1,1 +1,16 @@
+import Papa from "papaparse";
 
+export async function fetchSheet(url:string) {
+
+const res = await fetch(url)
+const text = await res.text()
+
+return new Promise((resolve) => {
+Papa.parse(text,{
+header:true,
+complete:(results)=>{
+resolve(results.data)
+}
+})
+})
+}
